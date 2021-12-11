c88567a799ccdceac6674a6ce1a0445bbc739075
sandfishfactory <sandfishfactory@gmail.com>
2021-12-11 21:23:19 +0900

    モジュール構成の変更

--- fastapi_websocket/src/chatapp/main.py
+++ fastapi_websocket/src/chatapp/main.py
@@ -7,7 +7,7 @@ from app.chat import router as chatRouter
 app = FastAPI()
 app.include_router(chatRouter)
 
-app.mount("/", StaticFiles(directory="front/public"), name="static")
+app.mount("/", StaticFiles(directory="public"), name="static")
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
